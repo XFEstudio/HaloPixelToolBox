8ef27f93d04e04fc8719f483cc06ad6f7a3d5713
XFEstudio <mail@xfegzs.com>
2025-12-04 16:06:58 +0800

    修复歌词一段时间后不显示的问题 添加地址实时解析功能

--- HaloPixelToolBox/HaloPixelToolBox.Test/Program.cs
+++ HaloPixelToolBox/HaloPixelToolBox.Test/Program.cs
@@ -12,14 +12,5 @@ internal class Program
     {
         var device = new HaloPixelDevice();
         device.Initialize();
-        device.SetUIModel(HaloPixelUIModel.Clock);
-        device.SetUIModel(HaloPixelUIModel.Game);
-        device.SetUIModel(HaloPixelUIModel.Work);
-        device.SetUIModel(HaloPixelUIModel.Read);
-        device.SetUIModel(HaloPixelUIModel.Cats);
-        device.SetUIModel(HaloPixelUIModel.Dogs);
-        device.SetUIModel(HaloPixelUIModel.Memes);
-        device.SetUIModel(HaloPixelUIModel.Cyber);
-        device.SetUIModel(HaloPixelUIModel.Waves);
     }
 }

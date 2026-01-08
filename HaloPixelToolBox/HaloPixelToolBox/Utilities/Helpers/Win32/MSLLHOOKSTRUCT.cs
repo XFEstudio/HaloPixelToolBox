1eec1b6508dccb56d1f1d0aaadeec1618e8fc7d0
XFEstudio <mail@xfegzs.com>
2026-01-09 03:46:13 +0800

    完善部分内容

---
+++ HaloPixelToolBox/HaloPixelToolBox/Utilities/Helpers/Win32/MSLLHOOKSTRUCT.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HaloPixelToolBox.Utilities.Helpers;
+
+public static partial class Win32Helper
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public uint mouseData;
+        public uint flags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+}
---
+++ HaloPixelToolBox/HaloPixelToolBox/Utilities/Helpers/Win32/POINT.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HaloPixelToolBox.Utilities.Helpers.Win32;
+
+public static partial class Win32Helper
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int x;
+        public int y;
+    }
+}
